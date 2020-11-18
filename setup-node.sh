b5b4a426cebed7f1b24f5569dbff0857c0ed4ad1
Nick Cabatoff <ncabatoff@hashicorp.com>
2020-11-18 15:36:10 -0500

    Add prometheus telemetry support.

--- setup-node.sh
+++ setup-node.sh
@@ -44,6 +44,13 @@ api_addr = "http://0.0.0.0:8200"
 listener "tcp" {
  address     = "0.0.0.0:8200"
  tls_disable = 1
+ telemetry {
+   unauthenticated_metrics_access = "true"
+ }
+}
+telemetry {
+  prometheus_retention_time = "30s"
+  disable_hostname = true
 }
 EOF
 
